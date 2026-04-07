2b91613e6ab435277b4674e619e28a937e349ed6
armaanhussains119-blip <armaan.hussain.s.119@kalvium.community>
2026-04-07 12:30:20 +0530

    Fix: restore index.html and fix routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import HomePage from "./components/HomePage";
-import CategoryPage from "./components/CategoryPage";
-import CartPage from "./components/CartPage";
-import CartSidebar from "./components/CartSidebar";
-import { products } from "./data/products";
-import { useState } from "react";
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import CartPage from './components/CartPage'
+import CategoryPage from './components/CategoryPage'
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-
   return (
-    <BrowserRouter>
-      <Header
-        searchTerm={searchTerm}
-        onSearchChange={setSearchTerm}
-      />
-
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage products={products} searchTerm={searchTerm} />}
-        />
-
-        <Route
-          path="/category/:category"
-          element={<CategoryPage products={products} />}
-        />
-
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-
-      <CartSidebar />
-    </BrowserRouter>
-  );
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/category/:category" element={<CategoryPage />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { CartProvider } from "./context/CartProvider.jsx";
+import { BrowserRouter } from "react-router-dom";   // ✅ ADD THIS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <BrowserRouter>   {/* ✅ ADD THIS */}
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
